e25b2a4e5124a7d0720e8f3c41dee50e745b0ea8
Ngoza Mayaka <ngozamay@gmail.com>
2024-08-15 22:29:16 +0200

    workingon rails watch list

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,29 @@
+class BookmarksController < ApplicationController
+  def new
+    @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
+    @movie = Movie.find(params[:movie_id])
+    @bookmark.list = @list
+    @bookmark.movie = @movie
+    @bookmark.save
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list), status: :see_other
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:list_id, :movie_id)
+  end
+
+
+end
